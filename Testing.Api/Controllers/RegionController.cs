6251061706c05a41b7c4234cb36c8631c35d3ea9
ahadbaloxh <abdulahad.personall@gmail.com>
2026-06-16 00:45:18 +0500

    Implement Domain  Modals & DTOs

--- Testing.Api/Controllers/RegionController.cs
+++ Testing.Api/Controllers/RegionController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Testing.Api.Data;
+using Testing.Api.Models.DTO;
 
 namespace Testing.Api.Controllers
 {
@@ -15,13 +16,57 @@ namespace Testing.Api.Controllers
             this.dbContext = dbContext;
         }
 
+
         // GET: api/Region
         [HttpGet]
         public IActionResult GetAll()
         {
             var regions = dbContext.Regions.ToList();
 
-            return Ok(regions);
+            // Map Domain Models to DTOs
+
+            var regionDtos = new List<RegionDto>();
+
+            foreach (var region in regions)
+            {
+                regionDtos.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+
+
+            // return DTOs
+            return Ok(regionDtos);
+        }
+
+
+        // GET: api/Region/{id}
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null)
+            {
+                return NotFound();
+            }
+
+
+            // Map entity to DTO
+            var regionDto = new RegionDto()
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+
+            return Ok(regionDto);
         }
     }
 }
